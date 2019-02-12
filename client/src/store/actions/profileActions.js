e0acf8a62b8549a9f4890a87b016930e2dd70d64
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-02-11 22:37:36 -0600

    user profile set up with redux

--- client/src/store/actions/profileActions.js
+++ client/src/store/actions/profileActions.js
@@ -27,6 +27,25 @@ export const getCurrentProfile = () => dispatch => {
     );
 };
 
+//GET PROFILE BY HANDLE
+export const getProfileByHandle = handle => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get(`/api/profile/handle/${handle}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: null
+      })
+    );
+};
+
 //CREATE NEW PROFILE
 export const createProfile = (profileData, history) => dispatch => {
   axios
