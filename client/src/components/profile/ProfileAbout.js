e885a74c647621a4b4113b9fdfe92c9f80a624f9
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-02-12 00:09:31 -0600

    bio, skills, experience, education

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -1,13 +1,50 @@
 import React, { Component } from "react";
+import isEmpty from "../../store/validation/is-Empty";
+import PropTypes from "prop-types";
 
 class ProfileAbout extends Component {
   render() {
+    const { profile } = this.props;
+
+    //GET FIRST NAME
+    const firstName = profile.user.name.trim().split(" ")[0];
+
+    //SKILL LIST
+    const skills = profile.skills.map((skill, index) => (
+      <div key={index} className="p-3">
+        <i className="fa fa-check">{skill}</i>
+      </div>
+    ));
     return (
-      <div>
-        <h1>Profile About</h1>
+      <div className="row">
+        <div className="col-md-12">
+          <div className="card card-body bg-light mb-3">
+            <h3 className="text-center text-info">{firstName}'s Bio</h3>
+
+            <p>
+              {isEmpty(profile.bio) ? (
+                <span>{firstName} does not have a bio</span>
+              ) : (
+                <span className="lead">{profile.bio}</span>
+              )}
+            </p>
+
+            <hr />
+            <h3 className="text-center text-info">Skill Set</h3>
+            <div className="row">
+              <div className="d-flex flex-wrap justify-content-center align-items-center">
+                {skills}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
+
+ProfileAbout.propTypes = {
+  profile: PropTypes.object.isRequired
+};
 
 export default ProfileAbout;
