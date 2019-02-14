470a2b71d0761c56b6b5e3ab54a8fb9c71526e98
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-02-14 17:55:09 -0600

    post feed component

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -2,16 +2,32 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import PostForm from "./PostForm";
+import PostFeed from "./PostFeed";
 import Spinner from "../../common/Spinner";
+import { getPost } from "../../store/actions/postActions";
 
 class Posts extends Component {
+  componentDidMount() {
+    this.props.getPost();
+  }
+
   render() {
+    const { posts, loading } = this.props.post;
+
+    let postContent;
+
+    if (posts === null || loading) {
+      postContent = <Spinner />;
+    } else {
+      postContent = <PostFeed posts={posts} />;
+    }
     return (
       <div className="feed">
         <div className="container">
           <div className="row">
             <div className="col-md-12">
               <PostForm />
+              {postContent}
             </div>
           </div>
         </div>
@@ -20,4 +36,16 @@ class Posts extends Component {
   }
 }
 
-export default connect()(Posts);
+Posts.propTypes = {
+  post: PropTypes.object.isRequired,
+  getPost: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  post: state.post
+});
+
+export default connect(
+  mapStateToProps,
+  { getPost }
+)(Posts);
--- client/src/store/actions/postActions.js
+++ client/src/store/actions/postActions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import { ADD_POST, GET_ERRORS } from "./types";
+import { ADD_POST, GET_ERRORS, GET_POSTS, POST_LOADING } from "./types";
 
 // ADD POST
 
@@ -19,4 +19,30 @@ export const addPost = postData => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+export const getPost = () => dispatch => {
+  dispatch(setPostLoading());
+  axios
+    .get("/api/posts")
+    .then(res =>
+      dispatch({
+        type: GET_POSTS,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_POSTS,
+        payload: null
+      })
+    );
+};
+
+//SET LOADING STATE
+
+export const setPostLoading = () => {
+  return {
+    type: POST_LOADING
+  };
 };
