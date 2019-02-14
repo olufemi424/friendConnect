3451f23cf999daa4051e12bdc1adf12b94997ce7
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-02-14 16:32:32 -0600

    post action and reducer

--- client/src/store/reducers/rootReducer.js
+++ client/src/store/reducers/rootReducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 import authReducer from "./authReducers";
 import errosReducers from "./errosReducers";
 import profileReducer from "./profileReducer";
+import postReducer from "./profileReducer";
 
 //COMBINE STORE STATE
 export default combineReducers({
   auth: authReducer,
   errors: errosReducers,
-  profile: profileReducer
+  profile: profileReducer,
+  post: postReducer
 });
