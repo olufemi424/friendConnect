f64856e9f34888886c9bf49e4a3bcff87f44b13b
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-03-28 06:59:50 -0400

    prepare for deploy

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 //server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
